519eb6bb911b6ee4cd074e454767da91fb26f81a
saikrishnapuppala553 <saikrishnapuppala553@gmail.com>
2025-11-16 22:59:46 +0530

    Designed Home Screen for teacher with options to add polls and view polls

---
+++ app/src/main/java/saikrishnas3495275/pollingapp/madproject/HomeActivity.kt
@@ -0,0 +1,13 @@
+package saikrishnas3495275.pollingapp.madproject
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
--- app/src/main/java/saikrishnas3495275/pollingapp/madproject/RegisterActivity.kt
+++ app/src/main/java/saikrishnas3495275/pollingapp/madproject/RegisterActivity.kt
@@ -104,7 +104,6 @@ fun OptInScreen() {
 
                 }
 
-
             }
 
             // Login Form Section
